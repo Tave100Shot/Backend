192bf84766f366e92d11cd71392fd72ab5e9f3be
hyeonbin <basilla0617@naver.com>
2024-03-31 17:58:21 +0900

    feat: NewsLetter Dto (#85)

---
+++ src/main/java/com/api/TaveShot/domain/news_letter/dto/NewsLetterRequestDto.java
@@ -0,0 +1,32 @@
+package com.api.TaveShot.domain.news_letter.dto;
+
+import com.api.TaveShot.domain.news_letter.domain.SubscriptionType;
+import com.api.TaveShot.domain.post.post.domain.PostTier;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NewsLetterRequestDto {
+
+    @Schema(description = "구독자 닉네임", example = "구독자 닉네임 예시")
+    @NotEmpty
+    private String nickname;
+
+    @Schema(description = "구독자 이메일", example = "구독자 이메일 예시")
+    @NotEmpty
+    private String email;
+
+    @Schema(description = "구독 유형", example = "'Technology' or 'Job'")
+    @NotEmpty
+    private String subscriptionType;
+
+    public SubscriptionType getSubscriptionType() {
+        // 문자열을 enum으로 변환
+        return SubscriptionType.findType(subscriptionType);
+    }
+}
---
+++ src/main/java/com/api/TaveShot/domain/news_letter/dto/NewsLetterResponseDto.java
@@ -0,0 +1,21 @@
+package com.api.TaveShot.domain.news_letter.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class NewsLetterResponseDto {
+    private Long id;
+    private String nickname;
+    private String email;
+    private String subscriptionType;
+
+    @Builder
+    public NewsLetterResponseDto(Long id, String nickname, String email,String subscriptionType){
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.subscriptionType = subscriptionType;
+    }
+}
