7102f1f9549d4b424a1f58b7a2a963c9ab45f4e5
hyeonbin <basilla0617@naver.com>
2024-03-31 17:57:52 +0900

    feat: NewsLetter Controller (#85)

---
+++ src/main/java/com/api/TaveShot/domain/news_letter/controller/NewsLetterController.java
@@ -0,0 +1,22 @@
+package com.api.TaveShot.domain.news_letter.controller;
+
+import com.api.TaveShot.domain.news_letter.dto.NewsLetterRequestDto;
+import com.api.TaveShot.domain.news_letter.service.NewsLetterService;
+import com.api.TaveShot.global.success.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class NewsLetterController {
+    private final NewsLetterService newsLetterService;
+
+    @PostMapping("/newsletter")
+    public SuccessResponse<Long> subscribe(final NewsLetterRequestDto requestDto){
+        Long subscribeId = newsLetterService.subscribe(requestDto);
+        return new SuccessResponse<>(subscribeId);
+    }
+}
