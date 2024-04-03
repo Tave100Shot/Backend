f754b362103c1175467537cd9ba9db0f9a59a01c
hyeonbin <basilla0617@naver.com>
2024-04-03 21:49:38 +0900

    feat: Swagger API documentation 추가 (#85)

--- src/main/java/com/api/TaveShot/domain/news_letter/content/controller/NewsLetterContentController.java
+++ src/main/java/com/api/TaveShot/domain/news_letter/content/controller/NewsLetterContentController.java
@@ -3,7 +3,13 @@ package com.api.TaveShot.domain.news_letter.content.controller;
 import com.api.TaveShot.domain.news_letter.content.domain.NewsLetterContent;
 import com.api.TaveShot.domain.news_letter.content.dto.NewsLetterContentRequestDto;
 import com.api.TaveShot.domain.news_letter.content.service.NewsLetterContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewsLetterContentController {
 
+
     private final NewsLetterContentService newsLetterContentService;
 
+    @Operation(summary = "해당 뉴스레터 저장", description = "해당 뉴스레터를 저장합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "뉴스레터 저장 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Long.class)))
+    })
     @PostMapping("/content")
     public NewsLetterContent addContent(@RequestBody NewsLetterContentRequestDto contentRequestDto) {
         return newsLetterContentService.saveContent(contentRequestDto);
--- src/main/java/com/api/TaveShot/domain/news_letter/main/controller/NewsLetterController.java
+++ src/main/java/com/api/TaveShot/domain/news_letter/main/controller/NewsLetterController.java
@@ -3,7 +3,13 @@ package com.api.TaveShot.domain.news_letter.main.controller;
 import com.api.TaveShot.domain.news_letter.main.dto.NewsLetterRequestDto;
 import com.api.TaveShot.domain.news_letter.main.service.NewsLetterService;
 import com.api.TaveShot.global.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsLetterController {
     private final NewsLetterService newsLetterService;
 
+    @Operation(summary = "뉴스레터 구독", description = "뉴스레터를 구독합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "뉴스레터 구독 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Long.class)))
+    })
     @PostMapping("/newsletter")
     public SuccessResponse<Long> subscribe(@RequestBody final NewsLetterRequestDto requestDto){
         Long subscribeId = newsLetterService.subscribe(requestDto);
