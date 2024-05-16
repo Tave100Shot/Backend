949c97a80aead7c19c2b02689ea8e485c0e6c4fc
toychip <dlawnsgud427@naver.com>
2024-05-16 20:29:47 +0900

    refactor: 뉴스레터 활성화 쿼리 추가 (#85)

--- src/main/java/com/api/TaveShot/domain/newsletter/repository/NewsletterRepositoryImpl.java
+++ src/main/java/com/api/TaveShot/domain/newsletter/repository/NewsletterRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.api.TaveShot.domain.newsletter.domain.LetterType;
 import com.api.TaveShot.domain.newsletter.domain.Newsletter;
 import com.api.TaveShot.global.exception.ApiException;
 import com.api.TaveShot.global.exception.ErrorType;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -26,11 +27,15 @@ public class NewsletterRepositoryImpl implements NewsletterRepositoryCustom {
                 .selectFrom(newsletter)
                 .where(
                         newsletter.id.eq(newsletterId),
-                        newsletter.deleted.isFalse()
+                        getActivated()
                 )
                 .fetchOne();
         return Optional.ofNullable(findNewsletter)
                 .orElseThrow(() -> new ApiException(ErrorType.NEWSLETTER_NOT_FOUND));
+    }
+
+    private BooleanExpression getActivated() {
+        return newsletter.deleted.isFalse();
     }
 
     @Override
@@ -41,6 +46,18 @@ public class NewsletterRepositoryImpl implements NewsletterRepositoryCustom {
         return new PageImpl<>(pageContent, pageable, count);
     }
 
+    @Override
+    public List<Newsletter> findRecent6(final Long limit) {
+        return jpaQueryFactory
+                .selectFrom(newsletter)
+                .where(
+                        getActivated()
+                )
+                .limit(limit)
+                .orderBy(newsletter.id.desc())
+                .fetch();
+    }
+
     private List<Newsletter> getPageContent(final List<LetterType> letterTypes, final String containWord,
                                             final Pageable pageable) {
         return jpaQueryFactory
@@ -48,7 +65,8 @@ public class NewsletterRepositoryImpl implements NewsletterRepositoryCustom {
                 .where(
                         newsletter.content.contains(containWord),
                         newsletter.title.contains(containWord),
-                        newsletter.letterType.in(letterTypes)
+                        newsletter.letterType.in(letterTypes),
+                        getActivated()
                 )
                 .limit(pageable.getPageSize())
                 .offset(pageable.getOffset())
@@ -63,7 +81,8 @@ public class NewsletterRepositoryImpl implements NewsletterRepositoryCustom {
                 .where(
                         newsletter.content.contains(containWord),
                         newsletter.title.contains(containWord),
-                        newsletter.letterType.in(letterTypes)
+                        newsletter.letterType.in(letterTypes),
+                        getActivated()
                 )
                 .fetchOne();
     }
