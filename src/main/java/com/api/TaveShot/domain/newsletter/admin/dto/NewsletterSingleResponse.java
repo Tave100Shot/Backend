c7599b4e859f538715fbb75178a0a34ec38497ed
toychip <dlawnsgud427@naver.com>
2024-05-16 15:08:27 +0900

    feat: 뉴스레터 Id, 전송 성공 여부 응답에 추가 (#85)

--- src/main/java/com/api/TaveShot/domain/newsletter/admin/dto/NewsletterSingleResponse.java
+++ src/main/java/com/api/TaveShot/domain/newsletter/admin/dto/NewsletterSingleResponse.java
@@ -8,6 +8,10 @@ import java.time.LocalDateTime;
 
 @Schema(description = "개별 뉴스레터의 상세 정보")
 public record NewsletterSingleResponse(
+
+        @Schema(description = "뉴스레터 id", example = "2")
+        Long newsletterId,
+
         @Schema(description = "뉴스레터 제목", example = "월간 개발 소식")
         String title,
 
@@ -18,7 +22,10 @@ public record NewsletterSingleResponse(
         LetterType letterType,
 
         @Schema(description = "작성 시간", example = "21.08.01 12:00")
-        String writtenTime
+        String writtenTime,
+
+        @Schema(description = "전송 성공 여부", example = "true")
+        boolean sendStatus
 ) {
 
     public static NewsletterSingleResponse from(final Newsletter newsletter) {
@@ -26,8 +33,9 @@ public record NewsletterSingleResponse(
         String writtenTime = TimeUtil.formatCreatedDate(modifiedDate);
 
         return new NewsletterSingleResponse(
-                newsletter.getTitle(), newsletter.getContent(),
-                newsletter.getLetterType(), writtenTime
+                newsletter.getId(), newsletter.getTitle(),
+                newsletter.getContent(), newsletter.getLetterType(),
+                writtenTime, newsletter.isSent()
         );
     }
 }
