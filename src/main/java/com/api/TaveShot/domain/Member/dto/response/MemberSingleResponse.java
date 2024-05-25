9eb716f89e44fbbadcdc132896a43b359c8fab9f
toychip <dlawnsgud427@naver.com>
2024-05-25 13:54:29 +0900

    feat: gitLoginId 필드 추가, Response 자체적 static factory method 사용 (#99)

--- src/main/java/com/api/TaveShot/domain/Member/dto/response/MemberSingleResponse.java
+++ src/main/java/com/api/TaveShot/domain/Member/dto/response/MemberSingleResponse.java
@@ -1,12 +1,17 @@
 package com.api.TaveShot.domain.Member.dto.response;
 
+import com.api.TaveShot.domain.Member.domain.Member;
+import com.api.TaveShot.domain.newsletter.client.domain.Subscription;
 import com.api.TaveShot.domain.newsletter.domain.LetterType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "개별 멤버의 상세 정보")
 public record MemberSingleResponse(
-        @Schema(description = "백준 사용자 이름", example = "bojUser123")
+        @Schema(description = "백준 이름", example = "bojUser123")
         String bojName,
+
+        @Schema(description = "GitHub 이름, 회원가입할 때의 유일한 id를 뜻함. 변경할 수 없는 ID", example = "toychip")
+        String gitLoginId,
 
         @Schema(description = "구독 상태", example = "true")
         boolean subStatus,
@@ -21,15 +26,21 @@ public record MemberSingleResponse(
         String email
 ) {
 
-    public static MemberSingleResponse of(
-            String inputBojName, LetterType inputLetterType, String inputEmail
-    ) {
-        String bojName = getBojName(inputBojName);
-        LetterType letterType = getLetterType(inputLetterType);
-        String email = getEmail(inputEmail);
+    public static MemberSingleResponse from(Member member) {
+        String bojName = getBojName(member.getBojName());
+        String gitLoginId = getGitLoginId(member.getGitLoginId());
+        LetterType letterType = getLetterType(member.getSubscription());
+        String email = getEmail(member.getGitEmail());
         boolean subStatus = getStatus(letterType);
-        boolean secondCertified = getSecondCertified(inputBojName);
-        return new MemberSingleResponse(bojName, subStatus, letterType, secondCertified, email);
+        boolean secondCertified = getSecondCertified(member.getBojName());
+        return new MemberSingleResponse(bojName, gitLoginId, subStatus, letterType, secondCertified, email);
+    }
+
+    private static String getGitLoginId(final String gitLoginId) {
+        if (gitLoginId == null || gitLoginId.isBlank()) {
+            return "x";
+        }
+        return gitLoginId;
     }
 
     private static String getBojName(String bojName) {
@@ -51,11 +62,11 @@ public record MemberSingleResponse(
         return true;
     }
 
-    private static LetterType getLetterType(LetterType letterType) {
-        if (letterType == null) {
+    private static LetterType getLetterType(Subscription subscription) {
+        if (subscription == null) {
             return LetterType.NONE;
         }
-        return letterType;
+        return subscription.getLetterType();
     }
 
     private static boolean getSecondCertified(String bojName) {
