d030c3829d2e1c7ee9717fa13e4023d8383e132d
hyeonbin <basilla0617@naver.com>
2024-05-28 17:44:55 +0900

    feat: NewsletterResponse에 뉴스레터 객체를 포함하도록 변경 (#96)

--- src/main/java/com/api/TaveShot/domain/newsletter/letter/dto/NewsletterResponse.java
+++ src/main/java/com/api/TaveShot/domain/newsletter/letter/dto/NewsletterResponse.java
@@ -1,13 +1,11 @@
 package com.api.TaveShot.domain.newsletter.letter.dto;
 
-import com.api.TaveShot.domain.newsletter.domain.LetterType;
+import com.api.TaveShot.domain.newsletter.domain.Newsletter;
 import io.swagger.v3.oas.annotations.media.Schema;
-
-import java.util.Map;
 
 @Schema(description = "주간 뉴스레터 생성 응답")
 public record NewsletterResponse(
-        @Schema(description = "뉴스레터 ID 맵")
-        Map<LetterType, Long> newsletterIds
+        Newsletter devNewsletter,
+        Newsletter employeeNewsletter
 ) {
 }
