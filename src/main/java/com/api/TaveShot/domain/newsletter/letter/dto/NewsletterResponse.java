58aa6ab951ca125fc48a35f1cc905b87bcf09395
hyeonbin <basilla0617@naver.com>
2024-05-28 13:14:00 +0900

    refactor: Map<LetterType, Long>을 사용하여 불필요한 리포지토리 호출 제거 (#96)

--- src/main/java/com/api/TaveShot/domain/newsletter/letter/dto/NewsletterResponse.java
+++ src/main/java/com/api/TaveShot/domain/newsletter/letter/dto/NewsletterResponse.java
@@ -1,13 +1,13 @@
 package com.api.TaveShot.domain.newsletter.letter.dto;
 
+import com.api.TaveShot.domain.newsletter.domain.LetterType;
 import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.Map;
 
 @Schema(description = "주간 뉴스레터 생성 응답")
 public record NewsletterResponse(
-        @Schema(description = "개발자 뉴스레터 ID")
-        Long devNewsletterId,
-
-        @Schema(description = "직원 뉴스레터 ID")
-        Long employeeNewsletterId
+        @Schema(description = "뉴스레터 ID 맵")
+        Map<LetterType, Long> newsletterIds
 ) {
 }
