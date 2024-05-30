12efcddc1a17b8b7a810a60cd98ce1ebf935915e
toychip <dlawnsgud427@naver.com>
2024-05-30 14:14:59 +0900

    fix: 뉴스레터를 생성하는 트랜잭션 후에 메일이 전송되도록 수정 (#96)

--- src/main/java/com/api/TaveShot/domain/newsletter/letter/listener/NewsletterEventListener.java
+++ src/main/java/com/api/TaveShot/domain/newsletter/letter/listener/NewsletterEventListener.java
@@ -2,6 +2,8 @@ package com.api.TaveShot.domain.newsletter.letter.listener;
 
 import com.api.TaveShot.domain.newsletter.client.service.EmailSenderService;
 import com.api.TaveShot.domain.newsletter.event.NewsletterCreatedEvent;
+import com.api.TaveShot.global.exception.ApiException;
+import com.api.TaveShot.global.exception.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 import jakarta.mail.MessagingException;
 
 import java.util.List;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -16,13 +20,20 @@ public class NewsletterEventListener {
     private final EmailSenderService emailSenderService;
 
     @Async
-    @EventListener
-    public void handleNewsletterCreatedEvent(NewsletterCreatedEvent event) throws MessagingException {
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleNewsletterCreatedEvent(NewsletterCreatedEvent event) {
         String content = event.getContent();
         List<String> recipientEmails = event.getRecipientEmails();
 
-        for (String email : recipientEmails) {
-            emailSenderService.sendEmail(email, event.getNewsletter().getTitle(), content);
-        }
+        recipientEmails
+                .forEach(email -> {
+                    try {
+                        emailSenderService.sendEmail(email, event.getNewsletter().getTitle(), content);
+                    } catch (MessagingException e) {
+                        throw new ApiException(ErrorType._EMAIL_SEND_FAILED_LISTENER);
+                    } catch (Exception e) {
+                        throw new ApiException(ErrorType._STATIC_ERROR_RUNTIME_EXCEPTION);
+                    }
+                });
     }
 }
